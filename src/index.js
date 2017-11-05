4216c251210f673d5857800c856d433f42f0cf8d
Torben Werner <torbinsky@gmail.com>
2017-11-05 14:51:16 -0800

    TW: Bridge tweaks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 
 // Canvas setup
 const canvasWidth = 960;
-const canvasHeight = 680;
+const canvasHeight = 580;
 
 const svg = d3.select('body')
     .append('svg')
@@ -61,16 +61,11 @@ function drawPlot(data){
     let yScale = d3.scaleLinear()
     .domain([0,maxY])
     .range([plotHeight,0]);
-
-    let curveFn = d3.line()
-    .x(d => xScale(d.x))
-    .y(d => yScale(d.y))
-    .curve(d3.curveCardinal);
-
+    
     // draw axis
     plotGroup.append("g")
     .call(d3.axisLeft(yScale))
-
+    
     plotGroup.append("g")
     .attr("transform", "translate(0,"+plotHeight+")")
     .call(d3.axisBottom(xScale).ticks(20));
@@ -78,28 +73,32 @@ function drawPlot(data){
     // Calculate curve data
     const getCurvePoints = (x,y,w) => {
         // console.log({
-        //     scaleX: xScale(x),
-        //     scaleY: yScale(y),
-        //     scale0: yScale(0),
-        // })
-        // console.log({ x, y, w })
-        return [
-            {x: x-w, y: 0},
-            {x: x, y:y},
-            {x: x+w, y:0}
+            //     scaleX: xScale(x),
+            //     scaleY: yScale(y),
+            //     scale0: yScale(0),
+            // })
+            // console.log({ x, y, w })
+            return [
+                {x: x-w, y: 0},
+                {x: x, y:y},
+                {x: x+w, y:0}
         ];
     }
     data = data.map(d => getCurvePoints(d.x, d.y, curveWidth))
     // console.log(data)
-
+    
     // draw curves
+    let curveFn = d3.line()
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y))
+    .curve(d3.curveCardinal);
+    let colorFn = d3.scaleOrdinal().range(d3.schemeCategory20c)
     let curveGroup = plotGroup.append("g");
     let curves = curveGroup.selectAll('path').data(data);
     curves.enter().append("path")
-    .attr("stroke", "blue")
-    .attr("stroke-width", 2)
-    .attr("fill", "none")
-    .attr('d', d => curveFn(d));
+    .attr("fill", d => colorFn(d.mirna))
+    .attr("fill-opacity", "0.3")
+    .attr('d', curveFn);
 
     curves.exit().remove();
 }
